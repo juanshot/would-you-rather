40c845a3825eb3612ad7955b25b0041ea6bffb5b
Juan Miguel Garcia <juan-miguel.garcia@aboutyou.com>
2020-03-31 22:08:57 +0200

    [ADD] initializing the users and questions

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import { handleInitialData } from "./store/actions/shared";
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+  render() {
+    return <div>app</div>;
+  }
 }
-
-export default App;
+const mapStateToProps = state => ({
+  authedUser: state.authedUser
+});
+export default connect(mapStateToProps)(App);
--- src/store/reducers/authedUser.js
+++ src/store/reducers/authedUser.js
@@ -1,6 +1,6 @@
 import { SET_AUTHED_USER } from "../actions/authedUser";
 
-export default function defaultUser(state = null, action) {
+export default function authedUser(state = null, action) {
   switch (action) {
     case SET_AUTHED_USER:
       return action.id;
